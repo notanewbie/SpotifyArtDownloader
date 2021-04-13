930304aa85a8231556da28b0ed2ddb473355a98b
imak101 <mftrenkler@gmail.com>
2021-04-12 20:31:18 -0600

    Added Python 3 port and updated README.md

---
+++ python3/main.py
@@ -0,0 +1,21 @@
+import urllib.request
+
+
+def returnURL(url):
+    returned = urllib.request.urlopen(url=url).read()
+    return returned
+
+
+def get_photo():
+    inputted_URL = input("Enter the URL of a playlist you would like to download the playlist cover for:\n")
+
+    caught = str(returnURL(inputted_URL))
+
+    formatted = caught.split('<meta property="og:image" content="')[1].split('"')[0]
+    return formatted
+
+
+if __name__ == '__main__':
+    print("\n-----------------\n\n\n" + get_photo() + "\n\n")
+
+    input("-----------------\n\nPress enter to exit the program.\n")
